c67740ee643d6cd82f9beec54d9f703edd7bfff1
mpatrikik <m.patrik01@gmail.com>
2025-02-12 15:57:13 +0100

    adding to DB with postman

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -4,10 +4,7 @@ package com.telusko.quizapp.controller;
 import com.telusko.quizapp.Question;
 import com.telusko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +23,11 @@ public class QuestionController {
     @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("addQuestion")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
     }
 
 }
