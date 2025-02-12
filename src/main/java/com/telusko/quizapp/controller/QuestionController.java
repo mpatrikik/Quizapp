20a01affad8e7ef7b79aac3b8eaf9b972b2cf330
mpatrikik <m.patrik01@gmail.com>
2025-02-12 15:29:15 +0100

    added category listing

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.telusko.quizapp.Question;
 import com.telusko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class QuestionController {
     @GetMapping("allQuestions")
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
     }
 
 }
--- src/main/java/com/telusko/quizapp/dao/QuestionDao.java
+++ src/main/java/com/telusko/quizapp/dao/QuestionDao.java
@@ -3,5 +3,9 @@ package com.telusko.quizapp.dao;
 import com.telusko.quizapp.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
 }
