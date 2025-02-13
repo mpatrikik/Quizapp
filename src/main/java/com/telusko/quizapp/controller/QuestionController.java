891f95a0c0bbd31e26809078894e69778cbcc4ae
mpatrikik <m.patrik01@gmail.com>
2025-02-13 11:16:59 +0100

    category listing fixed

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -5,6 +5,7 @@ import com.telusko.quizapp.Question;
 import com.telusko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
--- src/main/java/com/telusko/quizapp/dao/QuestionDao.java
+++ src/main/java/com/telusko/quizapp/dao/QuestionDao.java
@@ -2,9 +2,11 @@ package com.telusko.quizapp.dao;
 
 import com.telusko.quizapp.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
