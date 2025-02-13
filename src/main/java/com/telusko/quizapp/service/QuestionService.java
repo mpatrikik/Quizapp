59a0c683e9398183198418b6e71abc0666f824c7
mpatrikik <m.patrik01@gmail.com>
2025-02-13 13:59:00 +0100

    refactor: update QuestionController and QuestionService to use ResponseEntity for better HTTP response handling

--- src/main/java/com/telusko/quizapp/service/QuestionService.java
+++ src/main/java/com/telusko/quizapp/service/QuestionService.java
@@ -4,8 +4,11 @@ package com.telusko.quizapp.service;
 import com.telusko.quizapp.Question;
 import com.telusko.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,16 +16,26 @@ public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Question> getAllQuestions() {
-       return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
         questionDao.save(question);
-        return "Question added successfully";
+        return new ResponseEntity<>("Question added successfully", HttpStatus.CREATED);
     }
 }
